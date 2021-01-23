8d441e2ba1fdec6fd743654441b0bc831dadbe63
Luis Antonio <luisantoniovieira18@gmail.com>
2021-01-23 15:52:21 -0300

    excluindo pastas

--- CBGER_ws/build/visao/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luis/Humanoide/CBGER_ws/src/visao
-# Build directory: /home/luis/Humanoide/CBGER_ws/build/visao
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
