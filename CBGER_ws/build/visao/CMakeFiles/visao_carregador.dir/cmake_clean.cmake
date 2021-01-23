b0450205118941c741d5352c833fd73624ec7ce8
Luis Antonio <luisantoniovieira18@gmail.com>
2021-01-23 14:48:30 -0300

    ROS workspace finalizado

---
+++ CBGER_ws/build/visao/CMakeFiles/visao_carregador.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/luis/Humanoide/CBGER_ws/devel/lib/visao/visao_carregador"
+  "/home/luis/Humanoide/CBGER_ws/devel/lib/visao/visao_carregador.pdb"
+  "CMakeFiles/visao_carregador.dir/src/carregador.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/visao_carregador.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
