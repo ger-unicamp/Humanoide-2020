19cae50379f65f29b550ab09999754350549ff20
LuisaoBrents <lf.brentegani@gmail.com>
2021-01-26 20:44:37 -0300

    Workspace ROS atualizado

--- CBGER_ws/src/visao/src/carregador.cpp
+++ CBGER_ws/src/visao/src/carregador.cpp
@@ -6,8 +6,16 @@
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include "std_msgs/Bool.h"
 
 using namespace std;
+
+bool verifica = false;
+
+void stopCallback(const std_msgs::Bool& bottomStop)
+{
+    verifica = bottomStop.data;
+}
 
 void imageCallback(const sensor_msgs::ImageConstPtr &msg)
 {   
@@ -24,15 +32,17 @@ void imageCallback(const sensor_msgs::ImageConstPtr &msg)
 }
 
 int main(int argc, char **argv)
-{
-    if (argc != 2)
+{   
+    if (argc != 3)
     {
         cout << "agrv must have image topic name" << endl;
         exit(1);
     }
 
-    cout << argv[1] << endl;
-    string topicName(argv[1]);
+    string imageTopic = argv[1];
+    string msgTopic = argv[2];
+    cout << "Nome dos tópicos: " << imageTopic << " " << msgTopic << endl;
+    
     string nodeName("CBGER_carregador");
     int _argc=0;
     char **_argv = NULL;
@@ -41,10 +51,17 @@ int main(int argc, char **argv)
 
     cv::namedWindow("view");
     cv::startWindowThread();
-
     image_transport::ImageTransport it(nh);
-    image_transport::Subscriber sub = it.subscribe(topicName.c_str(), 1, imageCallback);
+    image_transport::Subscriber sub = it.subscribe(imageTopic.c_str(), 1, imageCallback);
+    ros::Subscriber sub2 = nh.subscribe(msgTopic, 1, stopCallback);
 
-    ros::spin();
-    cv::destroyWindow("view");
+    while(ros::ok())
+    {
+        if(verifica == true)
+        {
+            cv::destroyWindow("view");
+            ros::shutdown();
+        }
+        ros::spinOnce();
+    }
 }
